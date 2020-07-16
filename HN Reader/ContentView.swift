7f3734ac3fdaea7c7f4a462796ddbdeca2cd8b99
Peggy Wollenhaupt <isande@gmail.com>
2020-07-16 13:11:27 -0600

    initial commit

---
+++ HN Reader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HN Reader
+//
+//  Created by Peggy Wollenhaupt on 7/16/20.
+//  Copyright © 2020 Peggy Wollenhaupt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
