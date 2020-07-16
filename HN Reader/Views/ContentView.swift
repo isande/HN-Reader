7e20cc613f05cee2c5fbf8df0f8dcd7020d060c8
Peggy Wollenhaupt <isande@gmail.com>
2020-07-16 14:04:11 -0600

    displays title and points in list view from HN api

--- HN Reader/Views/ContentView.swift
+++ HN Reader/Views/ContentView.swift
@@ -9,12 +9,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
     var body: some View {
         NavigationView {
-            List(posts) { post in
-                Text(post.title)
+            List(networkManager.posts) { post in
+                HStack {
+                    Text(String(post.points))
+                    Text(post.title)
+                }
             }
         .navigationBarTitle("Hacker News Reader")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
         }
     }
 }
